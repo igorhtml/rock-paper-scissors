13fd6dd674ef601b92efdc67dc2e624716440de1
Igor Cesar <98989076+igorhtml@users.noreply.github.com>
2025-04-27 16:03:58 -0300

    Create getHumanChoice function
    Created the function that let the user choose his weapon. Also initialize the computer and user scores with 0.

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function getComputerChoice () {
     return Math.floor(Math.random() * 3 + 1);
 }
 
+function getUserChoice () {
+    return prompt("Escolha sua arma: \nPedra: 1\nPapel: 2\nTesoura: 3");
+}
+
+let humamScore = 0, computerScore = 0;
