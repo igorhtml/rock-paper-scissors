48fdc34a773e1f0ae2baae118761bd04e9a71571
Igor Cesar <98989076+igorhtml@users.noreply.github.com>
2025-04-27 16:24:32 -0300

    Create playRound function with round logic
    This function creates the user and computer variables with their choices, calling their choice functions. After, this function compare their weapons and log if is a win, lose or draw. Then, increment the scores.

--- script.js
+++ script.js
@@ -16,12 +16,59 @@ function getComputerChoice () {
 }
 
 function getUserChoice () {
-    let userChoice = prompt("Escolha sua arma (escreva):\n Pedra\nPapel\nTesoura");
+    let userChoice = prompt("Pick your weapon:\n Rock\nPaper\nScissors");
     return userChoice.toLowerCase();
 }
 
-let humamScore = 0, computerScore = 0;
+let userScore = 0, computerScore = 0;
 
 function playRound () {
+    let computerChoice = getComputerChoice();
+    let userChoice = getUserChoice();
+    console.log(computerChoice);
+    console.log(userChoice);
     
+    if(userChoice == "rock") {
+        if(computerChoice == "scissors") {
+            userScore +=1;
+            console.log("Computer picked scissors. You win this round!");
+        }
+        if(computerChoice == "paper") {
+            computerScore +=1;
+            console.log("Computer picked paper. You lose this round!");
+        }
+        if(computerChoice == "rock") {
+            console.log("Computer picked rock. Its a draw!");
+        }
+    }
+
+    if(userChoice == "scissors") {
+        if(computerChoice == "scissors") {
+            console.log("Computer picked scissors. Its a draw!");
+        }
+        if(computerChoice == "paper") {
+            userScore +=1;
+            console.log("Computer picked paper. You win this round!");
+        }
+        if(computerChoice == "rock") {
+            computerScore +=1;
+            console.log("Computer picked rock. You lose this round!");
+        }
+    }
+
+    if(userChoice == "paper") {
+        if(computerChoice == "scissors") {
+            computerScore +=1;
+            console.log("Computer picked scissors. You lose this round!");
+        }
+        if(computerChoice == "rock") {
+            userScore +=1;
+            console.log("Computer picked rock. You win this round!");
+        }
+        if(computerChoice == "paper") {
+            console.log("Computer picked paper. Its a draw!");
+        }
+    }
 }
+
+playRound();
